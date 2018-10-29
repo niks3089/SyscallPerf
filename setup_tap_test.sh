601f8d3feb2d2cca38ee65b595a6a5488e147f0f
nikhil <niks3089@gmail.com>
2018-10-29 18:00:56 -0400

    Add arp entries for tap tests

--- setup_tap_test.sh
+++ setup_tap_test.sh
@@ -3,4 +3,14 @@
 ./helper_scripts/create_tap.sh tap101 dummy1 br101 11.0.0.2 02:01:02:03:04:09
 ./helper_scripts/create_tap.sh tap102 dummy2 br102 12.0.0.2 02:01:02:03:04:10
 
+# Delete static arp entries
+sudo arp -d 10.0.0.100
+sudo arp -d 11.0.0.100
+sudo arp -d 12.0.0.100
+
+# Add static arp entries
+sudo  sudo arp -s 10.0.0.100 `cat /sys/class/net/dummy0/address`
+sudo  sudo arp -s 11.0.0.100 `cat /sys/class/net/dummy1/address`
+sudo  sudo arp -s 12.0.0.100 `cat /sys/class/net/dummy2/address`
+
 brctl show
