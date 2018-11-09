a22c665b76687e5b4150ff60fe08465afffb5102
nikhil <niks3089@gmail.com>
2018-11-09 12:42:46 -0500

    Run busyloop in network namespace

---
+++ busyloop_netns_multiplecpus.sh
@@ -0,0 +1,15 @@
+
+if [ $# -ne 2 ]; then
+    echo "busyloop_multiplecpus.sh <Number of programs> <start_cpu>"
+    exit 1
+fi
+
+curr_interface=0
+cpu=$2
+while [ $curr_interface -lt $1 ]
+do
+    curr_interface=`expr $curr_interface + 1`
+    echo Starting program to loop on cpu: $cpu
+    sudo ip netns exec dummy_netns$curr_interface ./busyloop $cpu &
+    cpu=`expr $cpu + 1`
+done
